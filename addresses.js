0810ba2990cdee98ca4bc7669ea6604395445ee8
emartinez1621 <emartinez@crestfinancial.com>
2016-02-10 11:53:14 -0700

    Basic template for array

---
+++ addresses.js
@@ -0,0 +1,25 @@
+{
+  "Alabama": [
+    {
+      "zip35004": [
+        {
+          [
+            "address1":
+            {
+              "street": "1936 Heritage Rd",
+              "city": "Moody"
+            }
+          ],
+          "address2": {
+            "street": "5011 Kelly Creek St",
+            "city": "Moody"
+          },
+          "address3": {
+            "street": "2407 Folson St",
+            "city": "Moody"
+          }
+        }
+      ]
+    }
+  ]
+}
