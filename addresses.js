d784c98574bc02ce48d0c49015f7b4f60f1158f9
emartinez1621 <emartinez@crestfinancial.com>
2016-02-10 14:31:01 -0700

    AK and AL

--- addresses.js
+++ addresses.js
@@ -1,3 +1,3 @@
 var validAddresses = '{' +
-    '"1" : [{"abbrev" : "AK", "zip" : "99507", "city" : "Anchorage", "address" : "8030 Sabrina St"}]' +
+    '"1" : [{"abbrev" : "AK", "zip" : "99507", "city" : "Anchorage", "address" : "8030 Sabrina St"}, {"abbrev" : "AK", "zip" : "99572", "city" : "Cooper Landing", "address" : "20518 Sterling Hwy"}, {"abbrev" : "AK", "zip" : "99683", "city" : "Trapper Creek", "address" : "25517 S Oilwell Rd"}]' +
 '}'
