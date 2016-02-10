a2e32f5ecdcc4bd3b6321371ceeb31cfe3804905
emartinez1621 <emartinez@crestfinancial.com>
2016-02-10 14:38:00 -0700

    AR

--- addresses.js
+++ addresses.js
@@ -1,3 +1,6 @@
 var validAddresses = '{' +
     '"1" : [{"abbrev" : "AK", "zip" : "99507", "city" : "Anchorage", "address" : "8030 Sabrina St"}, {"abbrev" : "AK", "zip" : "99572", "city" : "Cooper Landing", "address" : "20518 Sterling Hwy"}, {"abbrev" : "AK", "zip" : "99683", "city" : "Trapper Creek", "address" : "25517 S Oilwell Rd"}]' +
+    '"2" : [{"abbrev" : "AL", "zip" : "35004", "city" : "Moody", "address" : "1936 Heritage Rd"}, {"abbrev" : "AL", "zip" : "35071", "city" : "Gardendale", "address" : "1665 Quail Ridge Dr"}, {"abbrev" : "AL", "zip" : "35188", "city" : "Woodstock", "address" : "27 Old Stagecoach Rd"}]' +
+    '"3" : [{"abbrev" : "AR", "zip" : "71601", "city" : "Pine Bluff", "address" : "206 Park Pl"}, {"abbrev" : "AR", "zip" : "71765", "city" : "Strong", "address" : "2666 Jones Lake Rd"}, {"abbrev" : "AR", "zip" : "71866", "city" : "Winthrop", "address" : "2601 Highway 41 N"}]' +
+    
 '}'
