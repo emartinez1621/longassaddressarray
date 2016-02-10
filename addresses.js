addc3d44f9719f6e4a26f4d9aa057ba046bb849a
emartinez1621 <emartinez@crestfinancial.com>
2016-02-10 11:56:02 -0700

    Apparently didn't save. THIS is the real template

--- addresses.js
+++ addresses.js
@@ -1,25 +1,3 @@
-{
-  "Alabama": [
-    {
-      "zip35004": [
-        {
-          [
-            "address1":
-            {
-              "street": "1936 Heritage Rd",
-              "city": "Moody"
-            }
-          ],
-          "address2": {
-            "street": "5011 Kelly Creek St",
-            "city": "Moody"
-          },
-          "address3": {
-            "street": "2407 Folson St",
-            "city": "Moody"
-          }
-        }
-      ]
-    }
-  ]
-}
+var validAddresses = '{' +
+    '"1" : [{"abbrev" : "AK", "zip" : "99507", "city" : "Anchorage", "address" : "8030 Sabrina St"}]' +
+'}'
