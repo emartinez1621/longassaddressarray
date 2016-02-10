810e8ecc4f895f07f27ca34402c81f15362a9b06
emartinez1621 <emartinez@crestfinancial.com>
2016-02-10 14:46:47 -0700

    CA

--- addresses.js
+++ addresses.js
@@ -3,5 +3,6 @@ var validAddresses = '{' +
     '"2" : [{"abbrev" : "AL", "zip" : "35004", "city" : "Moody", "address" : "1936 Heritage Rd"}, {"abbrev" : "AL", "zip" : "35071", "city" : "Gardendale", "address" : "1665 Quail Ridge Dr"}, {"abbrev" : "AL", "zip" : "35188", "city" : "Woodstock", "address" : "27 Old Stagecoach Rd"}]' +
     '"3" : [{"abbrev" : "AR", "zip" : "71601", "city" : "Pine Bluff", "address" : "206 Park Pl"}, {"abbrev" : "AR", "zip" : "71765", "city" : "Strong", "address" : "2666 Jones Lake Rd"}, {"abbrev" : "AR", "zip" : "71866", "city" : "Winthrop", "address" : "2601 Highway 41 N"}]' +
     '"4" : [{"abbrev" : "AZ", "zip" : "85248", "city" : "Chandler", "address" : "1515 W Honeysuckle Ln"}, {"abbrev" : "AZ", "zip" : "85310", "city" : "Glendale", "address" : "23833 N 39th Ln"}, {"abbrev" : "AZ", "zip" : "85282", "city" : "Tempe", "address" : "2019 S Granada Dr"}]' +
+    '"5" : [{"abbrev" : "CA", "zip" : "93260", "city" : "Posey", "address" : "43228 Oak Dr."}, {"abbrev" : "CA", "zip" : "90210", "city" : "Beverly Hills", "address" : "1270 Shadow Hill Way"}, {"abbrev" : "CA", "zip" : "95206", "city" : "Stockton", "address" : "1708 Ralph Ave"}]' +
     '"#" : [{"abbrev" : "#", "zip" : "#", "city" : "#", "address" : "#"}, {"abbrev" : "#", "zip" : "#", "city" : "#", "address" : "#"}, {"abbrev" : "#", "zip" : "#", "city" : "#", "address" : "#"}]' +
 '}'
