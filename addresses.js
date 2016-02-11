50be4987cc1bb331f6e56d18c5215a59cb443e05
ed1621 <eduardo.giovanni1621@gmail.com>
2016-02-11 15:40:55 -0700

    LA

--- addresses.js
+++ addresses.js
@@ -17,5 +17,6 @@ var validAddresses = '{' +
     '"16" : [{"abbrev" : "IN", "zip" : "46140", "city" : "Greenfield", "address" : "762 Berry St"}, {"abbrev" : "IN", "zip" : "46201", "city" : "Indianapolis", "address" : "442 N Denny St"}, {"abbrev" : "IN", "zip" : "46327", "city" : "Hammond", "address" : "4042 Grover Ave"}]' +
     '"17" : [{"abbrev" : "KS", "zip" : "66006", "city" : "Baldwin", "address" : "402 Fremont St"}, {"abbrev" : "KS", "zip" : "66101", "city" : "Kansas City", "address" : "843 Ivandale St"}, {"abbrev" : "KS", "zip" : "66945", "city" : "Hanover", "address" : "111 S Hanover St"}]' +
     '"18" : [{"abbrev" : "KY", "zip" : "40071", "city" : "Taylorsville", "address" : "896 Fiddler Ln"}, {"abbrev" : "KY", "zip" : "40299", "city" : "Jefferson", "address" : "10301 Grand Ave"}, {"abbrev" : "KY", "zip" : "40371", "city" : "Salt Lick", "address" : "67 Railroad St"}]' +
+    '"19" : [{"abbrev" : "LA", "zip" : "70118", "city" : "New Orleans", "address" : "2632 Calhoun St"}, {"abbrev" : "LA", "zip" : "70437", "city" : "Folsom", "address" : "77050 Robinson Rd"}, {"abbrev" : "LA", "zip" : "70526", "city" : "Crowley", "address" : "814 N Avenue G"}]' +
     '"#" : [{"abbrev" : "#", "zip" : "#", "city" : "#", "address" : "#"}, {"abbrev" : "#", "zip" : "#", "city" : "#", "address" : "#"}, {"abbrev" : "#", "zip" : "#", "city" : "#", "address" : "#"}]' +
 '}'
